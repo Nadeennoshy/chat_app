385edd28c8c61ac1e38af353fb53a926a933b0da
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-05-25 13:16:26 +0300

    Initialize flutterFire

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:chat_app/views/chat_view.dart';
 import 'package:chat_app/views/login_view.dart';
 import 'package:chat_app/views/register.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp());
 }
 
